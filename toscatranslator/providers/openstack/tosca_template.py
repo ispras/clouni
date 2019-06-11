552bef46b694ae69f6f82143fe5e78c8dc81d01f
bura2017 <valeriya.shvetcova@phystech.edu>
2019-06-11 18:17:26 +0300

    not tested

--- toscatranslator/providers/openstack/tosca_template.py
+++ toscatranslator/providers/openstack/tosca_template.py
@@ -1,9 +1,6 @@
-import os
-
 from toscatranslator.providers.openstack.provider_resource import OpenstackProviderResource
 
 from toscatranslator.providers.common.tosca_template import ProviderToscaTemplate
-from toscatranslator.providers.openstack.translator_to_openstack import TRANSLATE_FUNCTION
 
 
 class OpenstackToscaTemplate(ProviderToscaTemplate):
@@ -14,6 +11,3 @@ class OpenstackToscaTemplate(ProviderToscaTemplate):
 
     def provider_resource_class(self, node):
         return OpenstackProviderResource(node)
-
-    def translate_functions(self):
-        return TRANSLATE_FUNCTION
