40fc22e22944e7297c6a775cdb127165a8aeabe0
bura2017 <valeriya.shvetcova@phystech.edu>
2019-06-11 17:09:34 +0300

    unnecessary

--- toscatranslator/providers/common/tosca_node_for_translate.py
+++
@@ -1,6 +0,0 @@
-class ToscaNodeForTranslate(object):
-    def __init__(self, name, template, facts):
-        # Available schema for template
-        self.template = dict(template)
-        self.name = str(name)
-        self.facts = dict(facts)
